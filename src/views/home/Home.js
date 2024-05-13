2b06e9b42d44f5a0fa102ac4dd8a3f1c2996663e
ayoub bouazi <114018977+bouaziayoub@users.noreply.github.com>
2024-05-13 08:51:49 +0200

    Put each card in array of objects...

--- src/views/home/Home.js
+++ src/views/home/Home.js
@@ -4,6 +4,23 @@ import "./Home.css";
 import Card from "../../components/card/Card";
 
 function Home() {
+     const cards = [
+      {
+        title: "Nuestros Servicios",
+        description: "Adéntrate en un universo de soluciones diseñadas exclusivamente para ti. Desde productos artesanales hasta servicios personalizados, nuestra amplia gama de ofertas está cuidadosamente seleccionada para satisfacer tus necesidades más exigentes. Descubre cómo podemos hacer tu vida más fácil y cómoda con nuestras opciones innovadoras y de alta calidad.",
+        image: "images/services.jpg",
+      },
+      {
+        title: "Nuestra Historia",
+        description: "Sumérgete en las páginas de nuestro pasado y descubre los capítulos que han dado forma a nuestra identidad. Desde nuestros modestos inicios hasta los hitos que hemos alcanzado, cada momento cuenta una historia de dedicación, superación y visión. Explora cómo hemos evolucionado a lo largo del tiempo, enfrentando desafíos y celebrando triunfos en el camino.",
+        image: "images/history.jpg",
+      },
+      {
+        title: "Nuestro Equipo",
+        description: "Conoce a las personas detrás de nuestra misión: un equipo diverso y talentoso unido por un propósito común. Desde nuestros líderes inspiradores hasta nuestros empleados de base, cada miembro de nuestro equipo aporta su pasión y experiencia para hacer realidad nuestra visión compartida. Descubre cómo colaboramos, innovamos y nos apoyamos mutuamente para lograr resultados excepcionales.",
+        image: "images/team.jpg",
+      },
+    ];
   return (
     <>
       <div className="home-bg">
@@ -16,21 +33,9 @@ function Home() {
 
       {/* Usar card atraves de importarla del components */}
       <div className="container-cards">
-        <Card
-          title="Nuestros Servicios"
-          description="Adéntrate en un universo de soluciones diseñadas exclusivamente para ti. Desde productos artesanales hasta servicios personalizados, nuestra amplia gama de ofertas está cuidadosamente seleccionada para satisfacer tus necesidades más exigentes. Descubre cómo podemos hacer tu vida más fácil y cómoda con nuestras opciones innovadoras y de alta calidad."
-          image="images/services.jpg"
-        />
-        <Card
-          title="Nuestra Historia"
-          description="Sumérgete en las páginas de nuestro pasado y descubre los capítulos que han dado forma a nuestra identidad. Desde nuestros modestos inicios hasta los hitos que hemos alcanzado, cada momento cuenta una historia de dedicación, superación y visión. Explora cómo hemos evolucionado a lo largo del tiempo, enfrentando desafíos y celebrando triunfos en el camino."
-          image="images/history.jpg"
-        />
-        <Card
-          title="Nuestro Equipo"
-          description="Conoce a las personas detrás de nuestra misión: un equipo diverso y talentoso unido por un propósito común. Desde nuestros líderes inspiradores hasta nuestros empleados de base, cada miembro de nuestro equipo aporta su pasión y experiencia para hacer realidad nuestra visión compartida. Descubre cómo colaboramos, innovamos y nos apoyamos mutuamente para lograr resultados excepcionales."
-          image="images/team.jpg"
-        />
+      {cards.map((card, index) => (
+        <Card key={index} title={card.title} description={card.description} image={card.image} />
+      ))}
       </div>
     </>
   );
